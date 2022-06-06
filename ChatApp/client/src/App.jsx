b5a116fd6ace08236a4920e6327bd91933dd1ccf
Rokyuto <93655294+Rokyuto@users.noreply.github.com>
2022-06-06 23:14:03 +0300

    Setup

---
+++ ChatApp/client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {StreamChat} from 'stream-chat'
+import {Chat} from 'stream-chat-react'
+import Cookies from 'universal-cookie' // To be able to Save User Information in the Browser
+
+// React Arrow Function with export
+const App = () => {
+  return (
+    <div>
+        <h1>Chat Messages App</h1>
+    </div>
+  )
+}
+
+export default App
--- ChatApp/client/src/index.js
+++ ChatApp/client/src/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './app';
+import App from './App';
+
+// App = App.jsx (Entire Project configuration) || Get element with id = 'root' from App (public\index.html)
+ReactDOM.render(<App />, document.getElementById('root')); // Render the application
+
