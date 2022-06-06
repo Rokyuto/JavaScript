1067497da162110f5452fd432c7d2542c83ba90c
Rokyuto <93655294+Rokyuto@users.noreply.github.com>
2022-06-06 23:17:01 +0300

    Initial Structure

--- ChatApp/client/src/App.jsx
+++ ChatApp/client/src/App.jsx
@@ -3,6 +3,8 @@ import {StreamChat} from 'stream-chat'
 import {Chat} from 'stream-chat-react'
 import Cookies from 'universal-cookie' // To be able to Save User Information in the Browser
 
+const apiKey = '' // Setup the Chat || the Key is from my Stream Profile (https://gstrm.io/js-mastery)
+
 // React Arrow Function with export
 const App = () => {
   return (
