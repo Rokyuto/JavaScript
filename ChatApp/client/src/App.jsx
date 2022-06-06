2bbc68183113b1c156f50cfcf8d51a63554aa15c
Rokyuto <93655294+Rokyuto@users.noreply.github.com>
2022-06-06 23:27:19 +0300

    Initial Structure

--- ChatApp/client/src/App.jsx
+++ ChatApp/client/src/App.jsx
@@ -5,11 +5,22 @@ import Cookies from 'universal-cookie' // To be able to Save User Information in
 
 const apiKey = '' // Setup the Chat || the Key is from my Stream Profile (https://gstrm.io/js-mastery)
 
+const client = StreamChat.getInstance(apiKey) // Call as Funtion and Pass in the api key
+
 // React Arrow Function with export
 const App = () => {
   return (
-    <div>
-        <h1>Chat Messages App</h1>
+    <div className="app_wrapper">
+        {/* Chat Contains Channel Container and Channel List Container */}
+        <Chat>
+            <ChannellListContainer>
+
+            </ChannellListContainer>
+
+            <ChannelContainer>
+
+            </ChannelContainer>
+        </Chat>
     </div>
   )
 }
