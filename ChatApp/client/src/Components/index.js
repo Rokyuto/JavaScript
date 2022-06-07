e1a0c6bc4142540e09ffed8c10c74333f8532b27
Rokyuto <93655294+Rokyuto@users.noreply.github.com>
2022-06-07 23:53:10 +0300

    Initial Commit

--- ChatApp/client/src/Components/index.js
+++
@@ -1,3 +0,0 @@
-// Export all Components from the folder automatically
-export { default as ChannelContainer } from './ChannelContainer'
-export { default as ChannelListContainer } from './ChannelListContainer'
