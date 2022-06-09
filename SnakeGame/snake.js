aafd1446449de87f680d8c1f5f445a563aa324b7
Rokyuto <93655294+Rokyuto@users.noreply.github.com>
2022-06-09 21:21:30 +0300

    Rebuild Repository structure

--- SnakeGame/snake.js
+++
@@ -1,9 +0,0 @@
-export const SNAKE_SPEED = 1; // Snake moves 1 times per second
-
-export function f_Update(){
-    console.log("Update called")
-}
-
-export function f_Draw(){
-    console.log("Draw called")
-}
