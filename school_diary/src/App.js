67b770e3e5cf6e373cd32e6dfefe798b651bf57a
Rokyuto <viktorvalerievasenov6@gmail.com>
2022-06-18 17:20:32 +0300

    Login-Register Buttons Click and Div Visibility

--- school_diary/src/App.js
+++ school_diary/src/App.js
@@ -1,8 +1,11 @@
 import logo from './uktc.svg';
 import './App.css';
-import React from 'react'
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  //const var name|, mechanic name | state - true/false
+  const [showLogin , setShowLogin] = useState(true); // Show/Hide Mechanic
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +13,17 @@ function App() {
         <div className="LogReg_Container"> {/* Login-Register Container */}
           <div className="LogReg_Buttons_Container">
             {/* Login-Register Buttons */}
-            <span className="login_button">Login</span>
-            <span className="register_button">Register</span>
+            <span className="login_button" onClick={() => setShowLogin(true)} >Login</span> {/* On Login Button Click => Show Login Container */}
+            <span className="register_button" onClick = {() => setShowLogin(false)} >Register</span>  {/* On Register Button Click => Hide Login Container */}
           </div>
-          <div className="LoginContainer" >
+
+          {/* By Default Show Login Container ; showLogin = true */}
+          { showLogin && <div className="LoginContainer" >
             <input type="text" id="studentName" className="studentInput" placeholder="Email or username" minLength="17"></input> {/* Student School Number Input Field */}
             <input type="password" id="studentPassword" className="studentInput" placeholder="Password" ></input> {/* Student Pasword Input Field */}
             <input type="submit" value="Login" id="studentSubmit" className="studentSubmit"></input>  {/* Login Button */}
-          </div>
+          </div> }
+
           <div className="RegisterContainer"></div>
         </div>
       </header>
