9494859d7b7a855a8db4f5902b1ff1dd3ac5b631
Rokyuto <viktorvalerievasenov6@gmail.com>
2022-06-18 12:50:37 +0300

    Setup Page App

---
+++ school_diary/src/App.js
@@ -0,0 +1,16 @@
+import logo from './uktc.svg';
+import './App.css';
+
+// Wesite Functionality
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="School-logo" alt="UKTC_Logo" /> {/* UKTC School Logo */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
