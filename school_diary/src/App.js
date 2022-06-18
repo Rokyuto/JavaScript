7f79ad7d43549a1397fe050bd933e4267d4569c2
Rokyuto <viktorvalerievasenov6@gmail.com>
2022-06-18 17:22:56 +0300

    Added Comments

--- school_diary/src/App.js
+++ school_diary/src/App.js
@@ -11,19 +11,21 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="School-logo" alt="UKTC_Logo" /> {/* UKTC School Logo */}
         <div className="LogReg_Container"> {/* Login-Register Container */}
+
           <div className="LogReg_Buttons_Container">
             {/* Login-Register Buttons */}
             <span className="login_button" onClick={() => setShowLogin(true)} >Login</span> {/* On Login Button Click => Show Login Container */}
             <span className="register_button" onClick = {() => setShowLogin(false)} >Register</span>  {/* On Register Button Click => Hide Login Container */}
           </div>
 
-          {/* By Default Show Login Container ; showLogin = true */}
-          { showLogin && <div className="LoginContainer" >
+          {/* Login Container */}
+          { showLogin && <div className="LoginContainer" > {/* By Default Show Login Container ; showLogin = true */}
             <input type="text" id="studentName" className="studentInput" placeholder="Email or username" minLength="17"></input> {/* Student School Number Input Field */}
             <input type="password" id="studentPassword" className="studentInput" placeholder="Password" ></input> {/* Student Pasword Input Field */}
             <input type="submit" value="Login" id="studentSubmit" className="studentSubmit"></input>  {/* Login Button */}
           </div> }
 
+          {/* Register Container */}
           <div className="RegisterContainer"></div>
         </div>
       </header>
