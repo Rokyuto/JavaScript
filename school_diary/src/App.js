8b0fa10ec0fbfb05620fc740a3942c4a17e3f172
Rokyuto <viktorvalerievasenov6@gmail.com>
2022-06-18 16:39:15 +0300

    CLEAR USELESS FILES

--- school_diary/src/App.js
+++ school_diary/src/App.js
@@ -1,9 +1,8 @@
 import logo from './uktc.svg';
 import './App.css';
-import React, {Component} from 'react'
+import React from 'react'
 
-class App extends Component{
-  render(){
+function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -24,7 +23,6 @@ class App extends Component{
       </header>
     </div>
   );
-  }
 }
 
 export default App;
