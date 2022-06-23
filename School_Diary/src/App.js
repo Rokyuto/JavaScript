71b39b88452dbcd7bb4af96246a85743263db226
TsMichov <cvetimichov38@gmail.com>
2022-06-24 00:53:20 +0300

    RouterNavHomeDashBord

--- School_Diary/src/App.js
+++ School_Diary/src/App.js
@@ -1,36 +1,19 @@
-import logo from './uktc.svg';
-import './App.css';
-import React, { useState } from 'react'
-
-
+import Home from "./Home";
+import Dashboard from "./mechanics/Pages/Dashboard";
+import{BrowserRouter as Router,Route,Switch} from "react-router-dom"
 const App = () => {
-  //const var name|, mechanic name | state - true/false
-  const [showLogin , setShowLogin] = useState(true); // Show/Hide Mechanic
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="School-logo" alt="UKTC_Logo" /> {/* UKTC School Logo */}
-        <div className="LogReg_Container"> {/* Login-Register Container */}
+   <div className="Container">
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/Dashboard" component={Dashboard}/>
 
-          <div className="LogReg_Buttons_Container">
-            {/* Login-Register Buttons */}
-            <span className="login_button" onClick={() => setShowLogin(true)} >Login</span> {/* On Login Button Click => Show Login Container */}
-            <span className="register_button" onClick = {() => setShowLogin(false)} >Register</span>  {/* On Register Button Click => Hide Login Container */}
-          </div>
+      </Switch>
+    </Router>
 
-          {/* Login Container */}
-          { showLogin && <div className="LoginContainer" > {/* By Default Show Login Container ; showLogin = true */}
-            <input type="text" id="studentName" className="studentInput" placeholder="Email or username" minLength="17"></input> {/* Student School Number Input Field */}
-            <input type="password" id="studentPassword" className="studentInput" placeholder="Password" ></input> {/* Student Pasword Input Field */}
-            <input type="submit" value="Login" id="studentSubmit" className="studentSubmit" onClick={() => } ></input> {/* Login Button */}
-          </div> }
-
-          {/* Register Container */}
-          <div className="RegisterContainer"></div>
-        </div>
-      </header>
-    </div>
+   </div>
   );
 }
 
---
+++ School_Diary/src/Home.js
@@ -0,0 +1,35 @@
+import logo from './uktc.svg';
+import React, { useState } from 'react'
+
+const Home = () => {
+  //const var name|, mechanic name | state - true/false
+  const [showLogin , setShowLogin] = useState(true); // Show/Hide Mechanic
+
+  return (
+    <div className="Home">
+      <header className="App-header">
+        <img src={logo} className="School-logo" alt="UKTC_Logo" /> {/* UKTC School Logo */}
+        <div className="LogReg_Container"> {/* Login-Register Container */}
+
+          <div className="LogReg_Buttons_Container">
+            {/* Login-Register Buttons */}
+            <span className="login_button" onClick={() => setShowLogin(true)} >Login</span> {/* On Login Button Click => Show Login Container */}
+            <span className="register_button" onClick = {() => setShowLogin(false)} >Register</span>  {/* On Register Button Click => Hide Login Container */}
+          </div>
+
+          {/* Login Container */}
+          { showLogin && <div className="LoginContainer" > {/* By Default Show Login Container ; showLogin = true */}
+            <input type="text" id="studentName" className="studentInput" placeholder="Email or username" minLength="17"></input> {/* Student School Number Input Field */}
+            <input type="password" id="studentPassword" className="studentInput" placeholder="Password" ></input> {/* Student Pasword Input Field */}
+            <input type="submit" value="Login" id="studentSubmit" className="studentSubmit"  ></input> {/* Login Button */}
+          </div> }
+
+          {/* Register Container */}
+          <div className="RegisterContainer"></div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
